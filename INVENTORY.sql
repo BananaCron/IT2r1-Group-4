bf063a98fb86305d3e70bc26b4345b8727788f99
BananaCron <116904267+BananaCron@users.noreply.github.com>
2023-12-07 13:15:58 +0800

    Add files via upload

--- INVENTORY.sql
+++ INVENTORY.sql
@@ -4,7 +4,7 @@ CREATE TABLE currentstock (
   PRIMARY KEY (productcode)
 ) ;
 
-INSERT INTO currentstock VALUES ('prod1',146),('prod2',100),('prod3',202),('prod4',172),('prod5',500),('prod6',500),('prod7',10),('prod8',20);
+INSERT INTO currentstock VALUES ();
 
 CREATE TABLE customers (
   cid int NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE customers (
   PRIMARY KEY (cid)
 );
 
-INSERT INTO customers VALUES (301,'vip1','John Seed','New York','9818562354'),(302,'vip2','Jacob Seed','Texas','9650245489'),(303,'std1','Ajay Kumar','Mumbai','9236215622'),(304,'std2','Astha Walia','Chandigarh','8854612478'),(306,'vip3','Madhu Chitkara','Chandigarh','9826546182');
+INSERT INTO customers VALUES ();
 
 CREATE TABLE products (
   pid int NOT NULL,
@@ -28,7 +28,7 @@ CREATE TABLE products (
   UNIQUE (productcode)
 );
 
-INSERT INTO products VALUES (111,'prod1','Laptop',85000,90000,'Dell'),(112,'prod2','Laptop',70000,72000,'HP'),(113,'prod3','Mobile',60000,64000,'Apple'),(114,'prod4','Mobile',50000,51000,'Samsung'),(121,'prod5','Charger',2000,2100,'Apple'),(122,'prod6','Mouse',1700,1900,'Dell'),(128,'prod7','Power Adapter',3000,3500,'Dell'),(129,'prod8','Smart Watch',15000,17000,'Apple');
+INSERT INTO products VALUES ();
 
 CREATE TABLE purchaseinfo (
   purchaseID int NOT NULL,
@@ -40,7 +40,7 @@ CREATE TABLE purchaseinfo (
   PRIMARY KEY (purchaseID)
 ) ;
 
-INSERT INTO purchaseinfo VALUES (1001,'sup1','prod1','Wed Jan 14 00:15:19 IST 2021',10,850000),(1002,'sup1','prod6','Wed Jan 14 00:15:19 IST 2021',20,34000),(1003,'sup2','prod3','Wed Jan 14 00:15:19 IST 2021',5,300000),(1004,'sup2','prod5','Wed Jan 14 00:15:19 IST 2021',5,10000),(1005,'sup3','prod2','Wed Jan 14 00:15:19 IST 2021',2,140000),(1006,'sup4','prod4','Wed Jan 14 00:15:19 IST 2021',2,100000),(1009,'sup2','prod3','Wed Sep 01 04:11:13 IST 2021',2,120000),(1010,'sup1','prod7','Wed Sep 01 04:25:06 IST 2021',10,30000),(1011,'sup2','prod8','Fri Sep 03 00:00:00 IST 2021',20,300000);
+INSERT INTO purchaseinfo VALUES ();
 
 CREATE TABLE users (
   uid int NOT NULL,
@@ -53,5 +53,5 @@ CREATE TABLE users (
   PRIMARY KEY (uid)
 ) ;
 
-INSERT INTO users VALUES (20,'Admin','Local','9876543210','root','root','ADMINISTRATOR')
+INSERT INTO users VALUES ()
 
